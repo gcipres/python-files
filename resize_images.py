abccfb5dab59064a623f5fed095fda1da16ffee2
Gerardo Ciprés <gcipgcu@gmail.com>
2025-03-25 18:59:55 -0600

    import numpy to round images

--- resize_images.py
+++ resize_images.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 def resize_image(image_path, mipmap, new_width, new_height, rounded = False):
     img = cv2.imread(image_path)
@@ -29,11 +30,11 @@ def resize_image(image_path, mipmap, new_width, new_height, rounded = False):
 
 # icons for android
 
-resize_image("icon.png", "ic_launcher_mdpi", 48, 48)
-resize_image("icon.png", "ic_launcher_hdpi", 72, 72)
-resize_image("icon.png", "ic_launcher_xhdpi", 96, 96)
-resize_image("icon.png", "ic_launcher_xxhdpi", 144, 144)
-resize_image("icon.png", "ic_launcher_xxxhdpi", 192, 192)
+# resize_image("icon.png", "ic_launcher_mdpi", 48, 48)
+# resize_image("icon.png", "ic_launcher_hdpi", 72, 72)
+# resize_image("icon.png", "ic_launcher_xhdpi", 96, 96)
+# resize_image("icon.png", "ic_launcher_xxhdpi", 144, 144)
+# resize_image("icon.png", "ic_launcher_xxxhdpi", 192, 192)
 
 resize_image("icon.png", "ic_launcher_round_mdpi", 48, 48, True)
 resize_image("icon.png", "ic_launcher_round_hdpi", 72, 72, True)
@@ -41,15 +42,15 @@ resize_image("icon.png", "ic_launcher_round_xhdpi", 96, 96, True)
 resize_image("icon.png", "ic_launcher_round_xxhdpi", 144, 144, True)
 resize_image("icon.png", "ic_launcher_round_xxxhdpi", 192, 192, True)
 
-resize_image("icon.png", "ic_launcher_foreground_mdpi", 108, 108)
-resize_image("icon.png", "ic_launcher_foreground_hdpi", 162, 162)
-resize_image("icon.png", "ic_launcher_foreground_xhdpi", 216, 216)
-resize_image("icon.png", "ic_launcher_foreground_xxhdpi", 324, 324)
-resize_image("icon.png", "ic_launcher_foreground_xxxhdpi", 432, 432)
+# resize_image("icon.png", "ic_launcher_foreground_mdpi", 108, 108)
+# resize_image("icon.png", "ic_launcher_foreground_hdpi", 162, 162)
+# resize_image("icon.png", "ic_launcher_foreground_xhdpi", 216, 216)
+# resize_image("icon.png", "ic_launcher_foreground_xxhdpi", 324, 324)
+# resize_image("icon.png", "ic_launcher_foreground_xxxhdpi", 432, 432)
 
 # splash for android
-resize_image("icon.png", "splashscreen_logo_mdpi", 288, 288)
-resize_image("icon.png", "splashscreen_logo_hdpi", 432, 432)
-resize_image("icon.png", "splashscreen_logo_xhdpi", 576, 576)
-resize_image("icon.png", "splashscreen_logo_xxhdpi", 864, 864)
-resize_image("icon.png", "splashscreen_logo_xxxhdpi", 1152, 1152)
+# resize_image("icon.png", "splashscreen_logo_mdpi", 288, 288)
+# resize_image("icon.png", "splashscreen_logo_hdpi", 432, 432)
+# resize_image("icon.png", "splashscreen_logo_xhdpi", 576, 576)
+# resize_image("icon.png", "splashscreen_logo_xxhdpi", 864, 864)
+# resize_image("icon.png", "splashscreen_logo_xxxhdpi", 1152, 1152)
